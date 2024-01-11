cd3ddd8874d475691b4248cab36b145b7383de10
Asger Jon Vistisen <asgerjon2@gmail.com>
2024-01-11 09:43:18 +0100

    Delete src/typinator directory
    The metaclass related functionality will be migrated to dedicated repository

--- src/typinator/__init__.py
+++
@@ -1,7 +0,0 @@
-"""The typinator module provides a collection of metaclass related
-classes."""
-#  MIT Licence
-#  Copyright (c) 2024 Asger Jon Vistisen
-from __future__ import annotations
-
-from ._meta_meta_class import MetaTypinator
--- src/typinator/_typinator.py
+++
@@ -1,4 +0,0 @@
-"""Typinator provides the core metaclass for the typinator module"""
-#  MIT Licence
-#  Copyright (c) 2024 Asger Jon Vistisen
-from __future__ import annotations
